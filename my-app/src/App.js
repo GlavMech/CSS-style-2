15811933d5384a0fc43d93cf168f7537f64bde01
GlavMech <165685784+GlavMech@users.noreply.github.com>
2025-04-07 18:33:31 +0500

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './CardComponent/Card';
+
+function App() {
+  return (
+    <div>
+      <h1>Сдучайное изображение с сайта picsum</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
